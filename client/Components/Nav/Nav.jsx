94f54a8919c03c4374b68020eff5d95cefa0f594
John Lukenoff <jlukenoff@gmail.com>
2018-11-06 10:45:43 +1100

    Adjust styling, initialize Nav component

---
+++ client/Components/Nav/Nav.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+// import PropTypes from 'prop-types';
+
+// import styles from './Nav.css';
+
+const Nav = props => (
+  <div>Nav Here</div>
+);
+
+// Nav.propTypes = {
+// };
+
+export default Nav;
