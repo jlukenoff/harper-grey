93f89c7c1fc7316ffe7a0192f661b1eca8085672
John Lukenoff <jlukenoff@gmail.com>
2018-11-06 18:04:22 +1100

    Add component templates

---
+++ client/Components/About/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+// import styles from './About.css';
+
+const About = props => (
+	<div>About Here</div>
+);
+
+// About.propTypes = {
+// };
+
+export default About;
---
+++ client/Components/Shop/Shop.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+// import styles from './Shop.css';
+
+const Shop = props => (
+  <div>Shop Here</div>
+);
+
+// Shop.propTypes = {
+// };
+
+export default Shop;
